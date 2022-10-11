f5a6abd7f2ac100a89b4d3110e9f909adfc5a4e3
JelleKruijssen <jellepapenhoven@live.nl>
2022-10-11 17:06:30 +0200

    opdracht 4 (#5)

--- relational-data-storage/opdracht-4/DS-opdracht-4.1.4.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.4.sql
@@ -1,3 +1,6 @@
-SELECT name, straat, huisnr, postcode FROM mhl_suppliers
-WHERE EXISTS (SELECT COUNT(*) FROM mhl_rubrieken INNER JOIN mhl_suppliers_mhl_rubriek_view WHERE mhl_suppliers.id=mhl_suppliers_mhl_rubriek_view.mhl_suppliers_ID AND mhl_suppliers_mhl_rubriek_view.mhl_rubriek_view_ID=mhl_rubrieken.id)
-HAVING COUNT(*)=1
+SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode FROM mhl_suppliers
+INNER JOIN mhl_yn_properties ON mhl_suppliers.id = mhl_yn_properties.supplier_ID
+    WHERE propertytype_ID IN (
+        SELECT id FROM mhl_propertytypes WHERE name = 'ook voor particulieren')
+    OR propertytype_ID IN (
+        SELECT id FROM mhl_propertytypes WHERE name = 'specialistische leverancier');
